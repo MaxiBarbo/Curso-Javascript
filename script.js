b88fdd5c6dcb257ff870866e6b8bd85183d91a8e
maxi barbo <maxi10lb@gmail.com>
2021-11-18 06:53:34 -0300

    3

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
+//Funcion para calcular cambio divisa relacion ars/Usd - ars/euro
+
+
+
+function valorDolar(moneda,resultado){
+
+    moneda = prompt("Ingrese dolar o euro").toLowerCase();
+    
+    resultado = parseInt(prompt("Ingrese Cantidad Deseada"));
+    
+
+    let resultadoDolar = multi(resultado,dolar);
+    let resultadoEuro = multi(resultado,euro);
+    iva1 = sumarIva(resultadoEuro);
+    iva2 = sumarIva(resultadoDolar);
+
+if  (isNaN(resultado)){
+    alert ("No ingresaste un numero")
+          
+}
+    if (moneda !=="dolar" && moneda !=="euro"){
+        alert ("No ingresaste ni euro ni dolar")
+
+    }
+    
+    else if (moneda === "dolar"){
+        pregunta = prompt("iva incluido? escribe si o no")
+
+        if(pregunta === "si"){
+
+        alert("Con Iva " + iva2 + "\n"
+        + "Sin Iva " + resultadoDolar)
+            return true
+        }
+        else if (pregunta === "no");{
+        alert("$" + resultadoDolar + " Ars");
+            
+        } 
+        
+    }
+    
+    else if(moneda === "euro"){
+
+        alert("$" + resultadoEuro + " Ars");
+        return true
+    }      
+     
+    else (false);{
+    alert("ingrese datos nuevamente")
+
+    }
+} 
+    
+    var dolar = 200;
+    var euro = 250;
+
+    function sumarIva(precioCosto){
+    return (precioCosto + (precioCosto * 0.21));
+    }
+
+    function multi(a,b){
+    return a*b;
+    }
+    
 
--- script3.js
+++ script3.js
@@ -1,38 +1,45 @@
 
-//
+// Funcion para calcular cambio divisa relacion ars/Usd - ars/euro
 
-function valorDolar(){
 
-    let moneda = prompt("Ingrese dolar o euro").toLowerCase();
-    let resultado = parseInt(prompt("Ingrese Cantidad Deseada"));
-    let resultadoDolar = multi(resultado,dolar);
-    let resultadoEuro = multi(resultado,euro);
 
-if  (isNaN(resultado)){
-    alert ("No ingresaste un numero")
-}
-if (moneda !=="dolar" && moneda !=="euro"){
-    alert ("No ingresaste ni euro ni dolar")
+// function valorDolar(moneda,resultado){
 
-}
-else if (moneda === "dolar"){
+//     moneda = prompt("Ingrese dolar o euro").toLowerCase();
+//     resultado = parseInt(prompt("Ingrese Cantidad Deseada"));
+
+//     let resultadoDolar = multi(resultado,dolar);
+//     let resultadoEuro = multi(resultado,euro);
+
+
+// if  (isNaN(resultado)){
+//     alert ("No ingresaste un numero")
+// }
+// if (moneda !=="dolar" && moneda !=="euro"){
+//     alert ("No ingresaste ni euro ni dolar")
+
+// }
+// else if (moneda === "dolar"){
     
-    alert(resultadoDolar + " Ars");
-    return true
-}
-else if(moneda === "euro"){
+//     alert("$" + resultadoDolar + " Ars");
+//     return true
+// }
+// else if(moneda === "euro"){
 
-   alert(resultadoEuro + " Ars");
-   return true
-}
-else (false);{
-alert("ingrese datos nuevamente")
+//    alert("$" + resultadoEuro + " Ars");
+//    return true
+// }
+// else if(pregunta === "si"){
+//     return 
+// }
+// else (false);{
+// alert("ingrese datos nuevamente")
 
- }
-}
-    var dolar = 200;
-    var euro = 250;
+//  }
+// }
+//     var dolar = 200;
+//     var euro = 250;
 
-    function multi(a,b){
-    return a*b;
-}
+//     function multi(a,b){
+//     return a*b;
+//     }
